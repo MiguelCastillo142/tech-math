4ece19f4134bdf3a9e0a2564243a2656d9d75ce6
Ariel <ariel.quezada06@inacapmail.cl>
2024-06-30 17:41:18 -0400

    Ejercicios hasta el 5
    Co-authored-by: MiguelCastillo142 <MiguelCastillo142@users.noreply.github.com>

--- techmath/urls.py
+++ techmath/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('',TemplateView.as_view(template_name="index.html"),name='index'),
     path('ejercicios/',TemplateView.as_view(template_name="lista.html"),name='lista'),
     path('1/',TemplateView.as_view(template_name="ejercicios/1.html"),name='1'),
+    path('2/',TemplateView.as_view(template_name="ejercicios/2.html"),name='2'),
+    path('3/',TemplateView.as_view(template_name="ejercicios/3.html"),name='3'),
+    path('4/',TemplateView.as_view(template_name="ejercicios/4.html"),name='4'),
+    path('5/',TemplateView.as_view(template_name="ejercicios/5.html"),name='5'),
+ 
 ]
